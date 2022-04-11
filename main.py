5af485bf3520b60cad39711bded2d611afca7d36
Raphael Mara <100629888+raph2876@users.noreply.github.com>
2022-04-11 22:19:50 +0000

    Changed the wording a bit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # The name function is meant to ask the user for a name and store the name.
 
 def welcome():
-  print("Hello!")
+  print("Welcome!")
 # this checks if the input is a name with only letters and no spaces. If it is not then it will ask for it to be entered again. If it is a name with only letters, then it will store the name as a variable and continue
   while True:
     n = input(" \n Please enter your name: \n --> ")
@@ -15,7 +15,7 @@ def welcome():
       print("|!"*26 + "|")
 #Welcome message
   print("|+-" *28 + "|")
-  print("\n Hello, {}! \n Welcome to Raphael's math quiz! \n This is a basic facts quiz that is on a time limit! \n The goal of this is for you to answer questions as quickly as posible! \n Please answer the quiz only in integers (whole numbers). \n (disclaimer: This quiz is not intended to offend anyone knowingly or unknowingly) \n".format(n))
+  print("\n Why Hello There, {}! \n Welcome to Raphael's math quiz! \n This is a basic facts quiz that is on a time limit! \n The goal of this is for you to answer questions as quickly as posible! \n Please answer the quiz only in integers (whole numbers). \n (disclaimer: This quiz is not intended to offend anyone knowingly or unknowingly) \n".format(n))
   print("|+-" *28 + "|")
 
 welcome()
