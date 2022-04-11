12b5125ed0a3ef25ff8d6876e44047efbde1238e
Raphael Mara <100629888+raph2876@users.noreply.github.com>
2022-04-11 22:07:03 +0000

    Mostly finished welcome function

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ def welcome():
   print("Hello!")
 # this checks if the input is a name with only letters and no spaces. If it is not then it will ask for it to be entered again. If it is a name with only letters, then it will store the name as a variable and continue
   while True:
-    n = input("Please enter your name: \n > ")
+    n = input(" \n Please enter your name: \n --> ")
     if n.isalpha():
       break
     else:
+#Error message
+      print("|!"*26 + "|")
       print("Please enter a name with only letters and no spaces")
-  print("Hello, {}! \n Welcome to Raphael's math quiz!".format(n))
+      print("|!"*26 + "|")
+#Welcome message
+  print("|+-" *28 + "|")
+  print("\n Hello, {}! \n Welcome to Raphael's math quiz! \n This is a basic facts quiz that is on a time limit! \n The goal of this is for you to answer questions as quickly as posible! \n Please answer only in integers (whole numbers). \n (disclaimer: This quiz is not intended to offend anyone knowingly or unknowingly) \n".format(n))
+  print("|+-" *28 + "|")
 
 welcome()
-  
-
-
