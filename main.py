054b452f8f9e744ee6bdbffb47b20272945c3dea
Raphael Mara <100629888+raph2876@users.noreply.github.com>
2022-04-11 22:11:28 +0000

    slightly improved welcome function

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ def welcome():
       break
     else:
 #Error message
+      print ("\n")
       print("|!"*26 + "|")
       print("Please enter a name with only letters and no spaces")
       print("|!"*26 + "|")
 #Welcome message
   print("|+-" *28 + "|")
-  print("\n Hello, {}! \n Welcome to Raphael's math quiz! \n This is a basic facts quiz that is on a time limit! \n The goal of this is for you to answer questions as quickly as posible! \n Please answer only in integers (whole numbers). \n (disclaimer: This quiz is not intended to offend anyone knowingly or unknowingly) \n".format(n))
+  print("\n Hello, {}! \n Welcome to Raphael's math quiz! \n This is a basic facts quiz that is on a time limit! \n The goal of this is for you to answer questions as quickly as posible! \n Please answer the quiz only in integers (whole numbers). \n (disclaimer: This quiz is not intended to offend anyone knowingly or unknowingly) \n".format(n))
   print("|+-" *28 + "|")
 
 welcome()
